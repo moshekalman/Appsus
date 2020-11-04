04be44179f9b4b0f4ad56cdddfa5dce748440cf8
moshekalman <mosheK.r8@gmail.com>
2020-11-04 22:38:32 +0200

    added coloring

--- apps/notes/cmps/saved-note.cmp.js
+++ apps/notes/cmps/saved-note.cmp.js
@@ -3,23 +3,29 @@ export default {
     props: ['note'],
     name: 'saved-note',
     template: `
-    <section class="saved-note flex column align-center">
+    <section class="saved-note flex column align-center" :style="{backgroundColor:bgColor, color}">
         <div class="saved-text">
             {{note.info.txt}}
         </div>
         <div class="edit-btns flex space-between align-center">
             <img @click="emitDelete(note.id)" src="./assets/svgs/trash-solid.svg" class="edit-btn">
             <img @click="emitEdit(note.id)" src="./apps/notes/assets/svgs/edit-solid.svg" class="edit-btn">
-            <label>
-            <img src="./apps/notes/assets/svgs/edit-solid.svg" class="edit-btn">
-                <input v-model="bgColor" @change="changeColor()" type="color" hidden>
-            </label>
+
+                <label class="relative-label flex just-center align-center">
+                    <img src="./apps/notes/assets/svgs/palette-solid.svg" class="edit-btn">
+                    <input v-model="bgColor" @change="onEmitChangeBgColor" type="color" class="color-input">
+                </label>
+                <label class="relative-label flex just-center align-center">
+                    <img src="./apps/notes/assets/svgs/paint-brush-solid.svg" class="edit-btn">
+                    <input v-model="color" @change="onEmitChangeColor" type="color" class="color-input">
+                </label>
         </div>
     </section>
     `,
     data() {
         return {
-            bgColor: null
+            bgColor: null,
+            color: null
         };
     },
     methods: {
@@ -29,8 +35,17 @@ export default {
         emitEdit(id) {
             this.$emit('onEditEv', id);
         },
-        changeColor() {
-            console.log('changed', this.bgColor);
+        onEmitChangeBgColor() {
+            this.$emit('emitBgcChange', { bgc: this.bgColor, id: this.note.id });
+        },
+        onEmitChangeColor() {
+            this.$emit('emitColorChange', { color: this.color, id: this.note.id });
+        }
+    },
+    created() {
+        if (this.note.style) {
+            if (this.note.style.bgc) this.bgColor = this.note.style.bgc;
+            if (this.note.style.color) this.color = this.note.style.color;
         }
     },
 };
