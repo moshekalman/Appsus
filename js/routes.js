359c4d9403d2244bf0b09ffaa9856f271d0d33a6
moshekalman <mosheK.r8@gmail.com>
2020-11-04 18:10:11 +0200

    editing notes for some more functions

--- js/routes.js
+++ js/routes.js
@@ -1,13 +1,13 @@
 import homePage from './pages/home-page.cmp.js';
 import notesApp from '../apps/notes/cmps/notes-app.cmp.js';
 import noteText from '../apps/notes/cmps/note-text.cmp.js';
-import emailApp from '../apps/email/email-app.cmp.js'
-import emailNavCmp from '../apps/email/cmps/email-nav.cmp.js';
-import emailListCmp from '../apps/email/cmps/email-list.cmp.js';
-import emailFilterCmp from '../apps/email/cmps/email-filter.cmp.js';
-import emailAddCmp from '../apps/email/cmps/email-add.cmp.js';
-import emailSaved from '../apps/email/cmps/email-saved.cmp.js'
-import emailSent from '../apps/email/cmps/email-sent.cmp.js'
+// import emailApp from '../apps/email/email-app.cmp.js'
+// import emailNavCmp from '../apps/email/cmps/email-nav.cmp.js';
+// import emailListCmp from '../apps/email/cmps/email-list.cmp.js';
+// import emailFilterCmp from '../apps/email/cmps/email-filter.cmp.js';
+// import emailAddCmp from '../apps/email/cmps/email-add.cmp.js';
+// import emailSaved from '../apps/email/cmps/email-saved.cmp.js'
+// import emailSent from '../apps/email/cmps/email-sent.cmp.js'
 
 // import notesApp from
 
@@ -15,23 +15,23 @@ const myRoutes = [{
         path: '/',
         component: homePage
     },
-    {
-        path: '/email',
-        component: emailApp,
-        children: [{
-                path: 'list',
-                component: emailListCmp
-            },
-            {
-                path: 'save',
-                component: emailSaved
-            },
-            {
-                path: 'sent',
-                component: emailSant
-            },
-        ]
-    },
+    // {
+    //     path: '/email',
+    //     component: emailApp,
+    //     children: [{
+    //             path: 'list',
+    //             component: emailListCmp
+    //         },
+    //         {
+    //             path: 'save',
+    //             component: emailSaved
+    //         },
+    //         {
+    //             path: 'sent',
+    //             component: emailSant
+    //         },
+    //     ]
+    // },
     {
         path: '/notes',
         component: notesApp,
