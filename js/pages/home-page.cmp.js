07400738f5e7fe6cca7237562abb18a42ffdb4e3
moshekalman <mosheK.r8@gmail.com>
2020-11-05 21:21:08 +0200

    end day 2

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,9 +1,34 @@
 
 
 export default {
-    template:`
-    <section>
-        <h2>HOME PAGE</h2>
+    template: `
+    <section class="home-page container">
+        <div class="hero-container">
+            <img src="./assets/imgs/hero.jpg"class="hero-img">
+            <h2 class="hero-title">APPSUS</h2>
+        </div>
+        <div class="home-links">
+                 <div>
+                     <div @click="goEmail" class="router-icn">
+                         <i class="far fa-envelope"></i>
+                     </div>
+                     <h3>Go To <span @click="goEmail">Email</span></h3>
+                </div>
+                <div>   
+                    <div @click="goNotes" class="router-icn">
+                        <i class="far fa-sticky-note"></i>
+                    </div>
+                    <h3>Go To <span @click="goNotes">Notes</span></h3>
+                </div>
+        </div>
     </section>
-    `
-}
+    `,
+    methods: {
+        goEmail(){
+            this.$router.push('/email')
+        },
+        goNotes(){
+            this.$router.push('/notes')
+        }
+    },
+};
