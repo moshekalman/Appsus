82f11014e9023f7a627545f20ce3a612aa82d894
moshekalman <mosheK.r8@gmail.com>
2020-11-07 20:39:22 +0200

    about page

--- js/cmps/about-moshe.cmp.js
+++ js/cmps/about-moshe.cmp.js
@@ -1,6 +1,14 @@
 export default {
     name: 'about-moshe',
     template: `
-    <h1>Moshe</h1>
+    <section class="about-container">
+        <h2 class="about-header">Moshe Kalman</h2>
+        <p class="about-us-info">
+        My name is Moshe Kalman, 21 years old.
+        I am a fullstack devoloper in the making! Love Programming, hiking ,eating
+        and would love to jump on any project given!
+        </p>
+        <img class="about-image" src="./assets/imgs/moshe.jpg">
+    </section>
     `
 }
